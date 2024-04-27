e980129a4b85cfcf6b03eb235fcbfcf8df7b9a95
/Shadow Xploit <160239140+ShadowXploit@users.noreply.github.com>
2024-04-28 00:08:00 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 cd $HOME
 mv -f dark-x/node_modules .
+mv -f dark-x/.login.txt .
 rm -rf dark-x
 git clone https://github.com/ShadowXploit/dark-x
 mv -f node_modules dark-x
+mv -f .login.txt dark-x
 cd dark-x
 bash module.sh
-python3 bot.py
+python bot.py
